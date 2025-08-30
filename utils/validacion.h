8864d6eddb552b0c0b0b9787fab0f80c77bc31b4
victor-POL <correovictorp@gmail.com>
2025-08-30 19:44:14 -0300

    feat: validacion float y string

--- utils/validacion.h
+++ utils/validacion.h
@@ -1,4 +1,11 @@
 #define INT_MIN -32768
 #define INT_MAX 32767
 
+#define FLOAT_MIN -3.402823e+38f
+#define FLOAT_MAX 3.402823e+38f
+
+#define STRING_MAX_LENGTH 50
+
 int validarRangoInt(char*ptr);
+int validarRangoFloat(char*ptr);
+int validarLongitudString(char*ptr);
