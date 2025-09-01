172a0d166f2dc01863962e0fa0422beeeaa80317
victor-POL <correovictorp@gmail.com>
2025-09-01 00:19:08 -0300

    refactor: sacar informe error validacion de la validacion

--- utils/validacion.c
+++ utils/validacion.c
@@ -7,20 +7,12 @@
 
 
 /* -------------------------------------------------------------------------- */
-/*                             VARIABLES EXTERNAS                             */
-/* -------------------------------------------------------------------------- */
-extern void informarErrorValidacion(char* name_token);
-
-
-
-/* -------------------------------------------------------------------------- */
 /*                                  FUNCIONES                                 */
 /* -------------------------------------------------------------------------- */
 int validarRangoInt(char*ptr)
 {
     int valueCasted = atoi(ptr);
     if (valueCasted < INT_MIN || valueCasted > INT_MAX) {
-        informarErrorValidacion("CONST_INT");
         return 1;
     }
     return 0;
@@ -30,7 +22,6 @@ int validarRangoFloat(char*ptr)
 {
     float valueCasted = atof(ptr);
     if (valueCasted < FLOAT_MIN || valueCasted > FLOAT_MAX) {
-        informarErrorValidacion("CONST_FLOAT");
         return 1;
     }
     return 0;
@@ -40,7 +31,6 @@ int validarLongitudString(char*ptr)
 {
     int length = strlen(ptr) - 2; // -2 para no contar las comillas de apertura y cierre
     if (length > STRING_MAX_LENGTH) {
-        informarErrorValidacion("CONST_STR");
         return 1;
     }
     return 0;
